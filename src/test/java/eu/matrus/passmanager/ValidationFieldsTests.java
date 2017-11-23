ade4601eef0de5c51b72cdbc9df1b00422b5bc82
Bogusz Przybyslawski <bogusz.przybyslawski@sofomo.eu>
2017-11-23 17:25:58 +0100

    Extend ExceptionResponse to support FieldError

---
+++ src/test/java/eu/matrus/passmanager/ValidationFieldsTests.java
@@ -0,0 +1,9 @@
+package eu.matrus.passmanager;
+
+import org.junit.Test;
+
+public class validationFieldsTests extends TestConfigurator {
+
+    @Test
+    public void testPasswordValidation() {}
+}
